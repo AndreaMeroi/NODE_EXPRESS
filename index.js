3929de055c829ffacf9d6976b373a62b89386e2a
AndreaMeroi <andreameroidublin@gmail.com>
2026-04-14 14:06:05 +0200

    add node+npm

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import axios from "axios"
 axios.get('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => {
         console.log(response.data);
-        console.log(chalk.bgGreen(response.data)); // utilizzo di chalk 
     })
     .catch(error => {
         console.log('Error fetching data', error);
