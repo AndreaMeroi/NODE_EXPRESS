30f9c5abad3b866b1f91693a8e4a18de64e94929
AndreaMeroi <andreameroidublin@gmail.com>
2026-04-16 10:30:11 +0200

    add params

--- 02_express_pizzeria/server.js
+++ 02_express_pizzeria/server.js
@@ -2,6 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const menu = [
+    {
+        name: 'margherita',
+        img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg',
+        ingredients: ['pomodoro', 'mozzarella']
+    },
+    {
+        name: 'marinara',
+        img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg',
+        ingredients: ['pomodoro', 'aglio', 'origano']
+    },
+    {
+        name: 'diavola',
+        img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg',
+        ingredients: ['pomodoro', 'mozzarella', 'salame piccante']
+    },
+    {
+        name: 'bufalina',
+        img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg',
+        ingredients: ['pomodoro', 'mozzarella di bufala']
+    },
+    {
+        name: '4 formaggi',
+        img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg',
+        ingredients: ['pomodoro', 'mozzarella', 'gorgonzola', 'parmigiano', 'ricotta']
+    }
+]
 // con questa riga attivo la possibilità di restituire asset statici (es. le immagini) usando .static
 app.use(express.static('public'))
 
@@ -16,14 +43,39 @@ app.get('/', (req, res) => {
 })
 
 app.get('/menu', (req, res) => {
-    const menu = [
-        {
-            name: 'andrea',
-            img: './public/fe7a9320-abaf-4e08-9080-202bd1f959b5.jpg'
-        }
-
-
-    ]
-
     res.json(menu)
 })
+
+
+app.get('/menu/:name', (req, res) => {
+
+    console.log(req.params);
+
+    //trovare un item(oggetto in qst caso) che econtiene una specifica pizza nella const menu 
+    const pizza_name = req.params.name
+    const pizza = menu.find((item) => item.name.toLocaleLowerCase() == pizza_name)
+
+    // attivo un messaggio d'errore da mostrare all'utente SE l'item non viene trovato
+    if (!pizza) {
+        res.json({
+            error: 'pizza not found'
+        })
+    } else {
+        res.json(pizza)
+    }
+
+})
+
+/*examples of dynamic routes segments
+'menu/margherita'
+'menu/diavola'
+'menu/bufalina'
+'menu/4_formaggi'
+'menu/marinara'
+
+'posts/1'
+'posts/2'
+'posts/3'
+'posts/4'
+'posts/5'
+*/
